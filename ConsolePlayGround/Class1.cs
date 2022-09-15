7f282732da43ac34544dea655601ab95812d12c2
deltamaze <wcpooley@gmail.com>
2022-09-15 12:06:52 -0500

    misc

---
+++ ConsolePlayGround/Class1.cs
@@ -0,0 +1,14 @@
+﻿// Code By Wpooley
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePlayGround
+{
+    internal class Class1
+    {
+    }
+}
