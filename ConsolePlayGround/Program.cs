3f9a95a66bc6ae6d9ee499b4014210378073e5df
deltamaze <wcpooley@gmail.com>
2022-09-18 09:52:06 -0500

    date playground

--- ConsolePlayGround/Program.cs
+++ ConsolePlayGround/Program.cs
@@ -3,6 +3,8 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var test = (new DateTime(2022, 10, 3) - DateTime.Now).TotalDays;
+        Console.WriteLine(test.ToString());
     }
 
 }
