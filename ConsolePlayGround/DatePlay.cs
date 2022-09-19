5eb5f5de2519b62bc9c7cf4b8b97f795feac9263
deltamaze <wcpooley@gmail.com>
2022-09-18 21:08:08 -0500

    date play

---
+++ ConsolePlayGround/DatePlay.cs
@@ -0,0 +1,19 @@
+﻿// Code By Wpooley
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsolePlayGround
+{
+    public static class DatePlay
+    {
+        public static double DaysBetweenTwoDates(DateTime d1, DateTime d2)
+        {
+            return (d1 - d2).TotalDays;
+        }
+    }
+}
--- ConsolePlayGround/Program.cs
+++ ConsolePlayGround/Program.cs
@@ -1,10 +1,12 @@
-﻿class Program
+﻿using ConsolePlayGround;
+
+class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         var test = (new DateTime(2022, 10, 3) - DateTime.Now).TotalDays;
-        Console.WriteLine(test.ToString());
+        Console.WriteLine(DatePlay.DaysBetweenTwoDates(new DateTime(2022, 10, 3), DateTime.Now);
     }
 
 }
