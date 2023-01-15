eda8ca658cace99d143e929406f4904dbfe7b503
deltamaze <wcpooley@gmail.com>
2023-01-15 12:48:27 -0600

    manga downloader chapterlistmodel

---
+++ MangaDownloader/ChapterListModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaDownloader
+{
+    public class Attributes
+    {
+        public string volume { get; set; }
+        public string chapter { get; set; }
+        public string title { get; set; }
+        public string translatedLanguage { get; set; }
+        public object externalUrl { get; set; }
+        public DateTime publishAt { get; set; }
+        public DateTime readableAt { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public int pages { get; set; }
+        public int version { get; set; }
+    }
+
+    public class Relationship
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Attributes attributes { get; set; }
+    }
+
+    public class Datum
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Attributes attributes { get; set; }
+        public IList<Relationship> relationships { get; set; }
+    }
+
+    public class ChapterListModel
+    {
+        public string result { get; set; }
+        public string response { get; set; }
+        public IList<Datum> data { get; set; }
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public int total { get; set; }
+    }
+}
--- MangaDownloader/ChapterSelect.cs
+++ MangaDownloader/ChapterSelect.cs
@@ -12,11 +12,20 @@ namespace MangaDownloader
 {
     public partial class ChapterSelect : Form
     {
-        public ChapterSelect()
+        readonly SessionDataContext context;
+        public ChapterSelect(SessionDataContext context)
         {
+            this.context = context;
             InitializeComponent();
             // get url from context
-            // build api url for chapters
+
+            int getChaptersOffset = 0;
+            int getChapterLimit = 96;
+            int getChapterTotal = 0; // update after call
+            int getChaptersFetched = 0; // increment by limit until > than Total
+
+            // pull manga id 
+            string mangaId = 
             // call api, and pull all chapters, this may require walking thourgh a few times to build list
             // once done present list in form
             // if error, write to log
